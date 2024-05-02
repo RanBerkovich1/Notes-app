6333f02da4eb239f747472a52fd25472ffdebace
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-05-02 16:21:22 +0300

    added color variants

--- src/_codux/boards/style-guide.board.tsx
+++ src/_codux/boards/style-guide.board.tsx
@@ -10,7 +10,6 @@ export default createBoard({
         <div className={Classnames(StyleGuide_board_module.root)}>
             <div className={StyleGuide_board_module.pageHeader}>
                 <h1 className={StyleGuide_board_module.TitleHeader}>Style Guide</h1>
-                <span className={StyleGuide_board_module.logo}>LOGO</span>
             </div>
             <div className={StyleGuide_board_module.SectionContainer}>
                 <div className={StyleGuide_board_module.SectionTitle}>
@@ -28,11 +27,12 @@ export default createBoard({
                 <div className={StyleGuide_board_module.FlexContainer}>
                     <div className={StyleGuide_board_module.FontFamilyContainer}>
                         <span className={StyleGuide_board_module.FontFamilyAa}>Aa</span>
-                        <span className={Classnames('pNormal')}>DM Sans</span>
+                        <span className={Classnames('pNormal')}>Wix Madefor Display </span>
                     </div>
                 </div>
             </div>
             <div className={StyleGuide_board_module.subsectionContainer}>
+                Warning
                 <h2 className={StyleGuide_board_module.subSectionTitle}>Main Heading / h1</h2>
                 <div className={StyleGuide_board_module.grid}>
                     <div>
@@ -162,247 +162,95 @@ export default createBoard({
                 </p>
             </div>
             <div className={StyleGuide_board_module.subsectionContainer}>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Primary / Blue</h2>
+                <h2 className={StyleGuide_board_module.subSectionTitle}>Primary / Grass</h2>
                 <div className={StyleGuide_board_module.FlexContainer}>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'b10')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'grass1')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            b10
+                            grass1
                             <br />
-                            #F0F5FF
                         </div>
                     </div>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'b20')}
+                            className={Classnames(
+                                StyleGuide_board_module.colorContainer,
+                                StyleGuide_board_module.grass2,
+                            )}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            b20
-                            <br />
-                            #2F6FED
+                            grass2 <br />
                         </div>
                     </div>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'b30')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'grass3')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            b30
+                            grass3
                             <br />
-                            #1D5BD6
                         </div>
                     </div>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'b40')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'grass4')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            b40
-                            <br />
-                            #1E4EAE{' '}
+                            grass4
+                            <br />{' '}
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'grass5')}
+                        />
+                        <div className={StyleGuide_board_module.subtitleValue}>
+                            grass5
+                            <br />{' '}
+                        </div>
+                    </div>
+                    <div>
+                        <div
+                            className={Classnames(
+                                StyleGuide_board_module.colorContainer,
+                                'grassdark',
+                            )}
+                        />
+                        <div className={StyleGuide_board_module.subtitleValue}>
+                            grassdark
+                            <br />{' '}
                         </div>
                     </div>
                 </div>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Secondary / Dark</h2>
+                <h2 className={StyleGuide_board_module.subSectionTitle}>Secondary / Sand</h2>
                 <div className={StyleGuide_board_module.FlexContainer}>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'd10')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'sand1')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            d10
+                            sand1
                             <br />
-                            #E0E0E8
                         </div>
                     </div>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'd12')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'sand2')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            d20
-                            <br /> #AFAFB6
+                            sand2
+                            <br />
                         </div>
                     </div>
                     <div>
                         <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'd30')}
+                            className={Classnames(StyleGuide_board_module.colorContainer, 'sand3')}
                         />
                         <div className={StyleGuide_board_module.subtitleValue}>
-                            d30
+                            sand3
                             <br />
-                            #808087
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'd40')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            d40
-                            <br />
-                            #2B2B31{' '}
-                        </div>
-                    </div>
-                </div>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Warning </h2>
-                <div className={StyleGuide_board_module.FlexContainer}>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'w10')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            w10
-                            <br />
-                            #FEF4E9
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'w20')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            w20
-                            <br />
-                            #FCD0A0
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'w30')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            w30
-                            <br />
-                            ##FABF7A
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'w40')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            w40
-                            <br />
-                            #2B2B31{' '}
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'w50')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            w40
-                            <br />
-                            #AA661E{' '}
-                        </div>
-                    </div>
-                </div>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Error</h2>
-                <div className={StyleGuide_board_module.FlexContainer}>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'e10')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            e10
-                            <br />
-                            #feecec
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'e20')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            e20
-                            <br />
-                            #fab2af
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'e30')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            e30
-                            <br />
-                            #f26561
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'e40')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            e40
-                            <br />
-                            #ec6d69{' '}
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 'e50')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            e40
-                            <br />
-                            #a5302c{' '}
-                        </div>
-                    </div>
-                </div>
-                <h2 className={StyleGuide_board_module.subSectionTitle}>Success</h2>
-                <div className={StyleGuide_board_module.FlexContainer}>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 's10')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            s0
-                            <br />
-                            #E8F8F1
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 's20')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            s20
-                            <br />
-                            #9FE1C2
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 's30')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            s30
-                            <br />
-                            #76D6AB
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 's40')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            s40
-                            <br />
-                            #76D6AB{' '}
-                        </div>
-                    </div>
-                    <div>
-                        <div
-                            className={Classnames(StyleGuide_board_module.colorContainer, 's50')}
-                        />
-                        <div className={StyleGuide_board_module.subtitleValue}>
-                            s50
-                            <br />
-                            #0F804E{' '}
                         </div>
                     </div>
                 </div>
