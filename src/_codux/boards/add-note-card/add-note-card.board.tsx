c257f5ae82a903d86d03d72da46351e36e013d28
Oleksii Letushev <oleksiile@wix.com>
2024-05-14 15:33:58 +0300

    update

--- src/_codux/boards/add-note-card/add-note-card.board.tsx
+++ src/_codux/boards/add-note-card/add-note-card.board.tsx
@@ -1,13 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import { AddNoteCard } from '../../../components/add-note-card/add-note-card';
-import { ComponentWrapper } from '../../board-wrappers/component-wrapper';
+import { FakeRouter } from '../../board-wrappers/fake-router';
 
 export default createBoard({
     name: 'AddNoteCard',
     Board: () => (
-        <ComponentWrapper>
+        <FakeRouter>
             <AddNoteCard />
-        </ComponentWrapper>
+        </FakeRouter>
     ),
-    isSnippet: true,
+    isSnippet: false,
 });
--- src/components/add-note-card/add-note-card.tsx
+++ src/components/add-note-card/add-note-card.tsx
@@ -1,21 +1,19 @@
 import classNames from 'classnames';
 import styles from './add-note-card.module.scss';
-import { Button } from '../button/button';
+import { LinkButton } from '../button/link-button';
 import { PlusIcon } from '@radix-ui/react-icons';
-import { useNavigate, Link } from 'react-router-dom';
 
 export interface AddNoteCardProps {
     className?: string;
 }
 
 export const AddNoteCard = ({ className }: AddNoteCardProps) => {
-    const navigate = useNavigate();
     return (
         <div className={classNames(styles.root, className)}>
-            <Button onClick={() => navigate('/add-note')}>
+            <LinkButton to="add-note">
                 <PlusIcon className={styles.icon} />
                 Add Note
-            </Button>
+            </LinkButton>
         </div>
     );
 };
