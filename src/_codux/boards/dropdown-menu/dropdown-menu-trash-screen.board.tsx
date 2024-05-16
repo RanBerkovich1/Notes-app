0f87485076beb6a8adf1ac92a1722bf1cdc72c9a
matan georgi <matang@wix.com>
2024-05-16 16:15:58 +0300

    remove board settings

--- src/_codux/boards/dropdown-menu/dropdown-menu-trash-screen.board.tsx
+++ src/_codux/boards/dropdown-menu/dropdown-menu-trash-screen.board.tsx
@@ -3,21 +3,14 @@ import { TrashDropdownMenu } from '../../../components/dropdown-menu/trash-dropd
 import { StoreContextProvider } from '../../../data-management/store-context-provider';
 import { FakeDataService } from '../../../data-management/fake-data/fake-data-service';
 
-
-
 export default createBoard({
     name: 'ContextMenu - Trash screen',
     Board: () => {
-        const storageService = new FakeDataService([]); 
+        const storageService = new FakeDataService([]);
         return (
             <StoreContextProvider value={storageService}>
                 <TrashDropdownMenu isPinned={false} id={'1'} />
             </StoreContextProvider>
         );
     },
-    isSnippet: true,
-    environmentProps: {
-        canvasHeight: 117,
-    },
 });
-
--- src/_codux/boards/dropdown-menu/dropdown-menu.board.tsx
+++ src/_codux/boards/dropdown-menu/dropdown-menu.board.tsx
@@ -3,21 +3,17 @@ import { HomeDropdownMenu } from '../../../components/dropdown-menu/home-dropdow
 import { StoreContextProvider } from '../../../data-management/store-context-provider';
 import { FakeDataService } from '../../../data-management/fake-data/fake-data-service';
 
-
-
 export default createBoard({
     name: 'ContextMenu - Home screen',
     Board: () => {
-        const storageService = new FakeDataService([]); 
+        const storageService = new FakeDataService([]);
         return (
             <StoreContextProvider value={storageService}>
                 <HomeDropdownMenu isPinned={false} id={'1'} description={''} title={''} />
             </StoreContextProvider>
         );
     },
-    isSnippet: true,
     environmentProps: {
-        canvasHeight: 117,
+        canvasWidth: 15,
     },
 });
-
