0e74b9b5973bbab685019dc6c184ccaa46d0e876
Ran Berkovich <155557865+RanBerkovich1@users.noreply.github.com>
2024-05-16 15:29:44 +0300

    removed theme and box from radix

--- src/_codux/boards/note/note.board.tsx
+++ src/_codux/boards/note/note.board.tsx
@@ -1,19 +1,14 @@
 import { createBoard } from '@wixc3/react-board';
 import { Note } from '../../../components/note/note';
-import { Theme } from '@radix-ui/themes';
 
 export default createBoard({
     name: 'Note',
-    Board: () => (
-        <Theme>
-            <Note />
-        </Theme>
-    ),
+    Board: () => <Note />,
     isSnippet: true,
     environmentProps: {
-        windowBackgroundColor: '#f1f1f1',
-        canvasHeight: 203,
-        canvasBackgroundColor: '#d0bebe',
+        windowBackgroundColor: '#e2e2e2',
+        canvasHeight: 182.8739159124199,
+        canvasBackgroundColor: '#ffffff',
         canvasWidth: 199.48546924805095,
         windowHeight: 474,
         windowWidth: 698,
--- src/components/note/note.tsx
+++ src/components/note/note.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import styles from './note.module.scss';
-import { Box } from '@radix-ui/themes';
 import StyleGuide_module from '../../styles/common/style-guide.module.scss';
 import Drawingpin from '../../assets/drawing pin.svg';
 import Dotshorizontal from '../../assets/dots horizontal.svg';
@@ -11,7 +10,6 @@ export interface NoteProps {
 
 export const Note = ({ className }: NoteProps) => {
     return (
-        <Box maxWidth="200 ">
             <div className={styles.noteCard}>
                 <div className={styles.cardIcon}>
                     <Drawingpin />
@@ -28,6 +26,5 @@ export const Note = ({ className }: NoteProps) => {
                     cases. I hope that’s ok by anyone who reads it, if there’s someone like that.
                 </p>
             </div>
-        </Box>
     );
 };
