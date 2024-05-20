2f1c363af756d3a751ebd89f0ee75f1d6827b273
Tetiana Mandziuk <tetianaman@wix.com>
2024-05-20 19:04:52 +0200

    section + scroll

--- src/components/trash-page/trash-page.tsx
+++ src/components/trash-page/trash-page.tsx
@@ -4,6 +4,13 @@ import { FakeNote } from '../notes/fake-note/fake-note';
 import { useNotesStore } from '../../data-management/use-notes-store';
 import { useEffect, useMemo, useState } from 'react';
 import { SearchInput } from '../search-input/search-input';
+import { Section } from '../section/section';
+import {
+    Root as ScrollAreaRoot,
+    Viewport as ScrollAreaViewport,
+    Scrollbar as ScrollAreaScrollbar,
+    Thumb as ScrollAreaThumb,
+} from '@radix-ui/react-scroll-area';
 
 export interface TrashPageProps {
     className?: string;
@@ -32,12 +39,20 @@ export const TrashPage = ({ className }: TrashPageProps) => {
                 placeholder="Search deleted notes"
                 onChange={setSearchString}
             />
-            <div className={styles.title}>Trash</div>
-            <div className={styles['notes-list']}>
-                {relevantNotes.map((note) => (
-                    <FakeNote note={note} key={note.id} />
-                ))}
-            </div>
+            <Section title="Trash">
+                <ScrollAreaRoot>
+                    <ScrollAreaViewport>
+                        <div className={styles['notes-list']}>
+                            {relevantNotes.map((note) => (
+                                <FakeNote note={note} key={note.id} />
+                            ))}
+                        </div>
+                    </ScrollAreaViewport>
+                    <ScrollAreaScrollbar orientation="vertical">
+                        <ScrollAreaThumb />
+                    </ScrollAreaScrollbar>
+                </ScrollAreaRoot>
+            </Section>
         </div>
     );
 };
