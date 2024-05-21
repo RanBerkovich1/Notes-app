2df68cb27375566f50e5d70cb7e69974015ea633
Oleksii Letushev <oleksiile@wix.com>
2024-05-21 14:24:08 +0300

    unused

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,6 @@ import { RouteObject } from 'react-router-dom';
 import { SiteWrapper } from './components/site-wrapper/site-wrapper';
 import { Deleted } from './components/deleted/deleted';
 import { Notes } from './components/notes/notes';
-import { NotePage } from './components/note-page/note-page';
 import { AddNotePage } from './components/add-note-page/add-note-page';
 import { EditNotePage } from './components/edit-note-page/edit-note-page';
 
@@ -14,7 +13,7 @@ export const routes: RouteObject[] = [
             { index: true, element: <Notes /> },
             { path: 'deleted', element: <Deleted /> },
             { path: 'add-note', element: <AddNotePage /> },
-            { path: 'notes/:noteId', element: <EditNotePage /> }
+            { path: 'notes/:noteId', element: <EditNotePage /> },
         ],
     },
 ];
