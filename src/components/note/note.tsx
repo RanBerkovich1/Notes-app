94033a8283b3d96ee50353b7d0dad1379d6de4e4
matan georgi <matang@wix.com>
2024-05-21 14:56:20 +0300

    changes

--- src/components/note/note.tsx
+++ src/components/note/note.tsx
@@ -2,29 +2,45 @@ import classNames from 'classnames';
 import styles from './note.module.scss';
 import StyleGuide_module from '../../styles/common/style-guide.module.scss';
 import Drawingpin from '../../assets/drawing pin.svg';
+import DrawingpinFilled from '../../assets/drawing-pin-filled.svg';
 import Dotshorizontal from '../../assets/dots horizontal.svg';
+import { useNotesStore } from '../../data-management/use-notes-store';
+import type { Note as NoteType } from '../../data-management/types';
+import { TrashDropdownMenu } from '../dropdown-menu/trash-dropdown-menu';
+import { HomeDropdownMenu } from '../dropdown-menu/home-dropdown-menu';
+import { useEffect } from 'react';
 
-export interface NoteProps {
-    className?: string;
-}
+export const Note = (note: Omit<NoteType, 'modifiedAt' | 'createdAt'>) => {
+    const { pinNote, unPinNote, syncNotes } = useNotesStore();
+    const { id, isPinned, description, deletedAt, title } = note;
 
-export const Note = ({ className }: NoteProps) => {
+    useEffect(() => {
+        syncNotes();
+    }, [isPinned]);
+
+    const handlePinClick = () => {
+        if (isPinned) {
+            console.log('unpin');
+            unPinNote(id);
+        } else {
+            console.log('pin');
+            pinNote(id);
+        }
+        console.log(isPinned);
+    };
+
     return (
         <div className={styles.noteCard}>
             <div className={styles.cardIcon}>
-                <Drawingpin />
-                <Dotshorizontal />
+                {!deletedAt && (
+                    <span onClick={handlePinClick}>
+                        {isPinned ? <DrawingpinFilled /> : <Drawingpin />}
+                    </span>
+                )}
+                {deletedAt ? <TrashDropdownMenu id={id} /> : <HomeDropdownMenu {...note} />}
             </div>
-            <h4 className={classNames(StyleGuide_module.cardTitle, styles.title)}>
-                This is a long long title that breaks into 2 rows
-            </h4>
-            <p className={StyleGuide_module.cardText}>
-                This is a description text of this example note. I’ll make it a little longer so we
-                can see how it looks in different cases. I hope that’s ok by anyone who reads it, if
-                there’s someone like that.This is a description text of this example note. I’ll make
-                it a little longer so we can see how it looks in different cases. I hope that’s ok
-                by anyone who reads it, if there’s someone like that.
-            </p>
+            <h4 className={classNames(StyleGuide_module.cardTitle, styles.title)}>{title}</h4>
+            <p className={StyleGuide_module.cardText}>{description}</p>
         </div>
     );
 };
